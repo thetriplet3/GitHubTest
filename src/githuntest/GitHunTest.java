b2cea827e753864c128320dba1ebf9903d8a8d77
aravindaperera49 <aravindaperera49@gmail.com>
2016-03-04 21:27:11 +0530

    Ara :D

--- src/githuntest/GitHunTest.java
+++ src/githuntest/GitHunTest.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package githuntest;
 
-/**
- *
- * @author Tharushan
- */
+
 public class GitHunTest {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hello GitHub!!");
         System.out.println("Update 1");
+        System.out.println("Ara");
     }
     
 }

f9fcf56986e2d73d754769b5f27911cb2ff656f6
NadyaNIS <nadyaimanthie@gmail.com>
2016-03-04 22:32:13 +0530

    Ns

---
+++ src/githuntest/Exp.java
@@ -0,0 +1,15 @@
+package githuntest;
+
+public class Exp {
+    String sv;
+    int sg;
+    double sd;
+    Exp(){
+    this.sv="gfjdk";
+    this.sg=45;
+    this.sd=45.23;
+    }
+    public void print(){
+        System.out.println("sv= "+this.sv+" sg= "+this.sg+" sd= "+this.sd);
+    }
+}
